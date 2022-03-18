7b37392730f31c56f3932ddc4f42786602e94e94
alejodev <alejodev2103@icloud.com>
2022-03-18 12:08:51 -0600

    version 0.0.0.5

--- src/components/Internet/InternetStyles.js
+++ src/components/Internet/InternetStyles.js
@@ -3,10 +3,14 @@ import styled from 'styled-components';
 export const NoteContainer = styled.div`
     position: block;
     width: 100%;
-    height: 170px;
+    height: 110px;
     top: 50px;
     align-items: center;
     background-color: #000;
+
+    @media (min-width: 300px) and (max-width: 800px) {
+        height: 190px;
+    }
 `;
 
 export const Advise = styled.h1`
